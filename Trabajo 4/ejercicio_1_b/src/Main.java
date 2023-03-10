aeabc2bb81270d20b3cbc43bab34b62a9ab9397b
Dario213987 <dariocrsng@gmail.com>
2023-03-10 07:02:13 -0300

    nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/uiDesigner.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/ejercicio_1_a.iml 	nuevos archivos: Trabajo 4/ejercicio_1_a/src/ejercicio_1_A.java 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/ejercicio_1_b.iml 	nuevos archivos: Trabajo 4/ejercicio_1_b/src/Main.java 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_3/ejercicio_3.iml 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_2/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_3/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_3/out.txt 	nuevos archivos: Trabajo 4/ejercicio_3/src/Main.java 	nuevos archivos: Trabajo 4/ejercicio_3/src/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/src/out.txt

---
+++ Trabajo 4/ejercicio_1_b/src/Main.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        if(args.length>=3) {
+            int[] numeros = new int[args.length-1];
+            for(int i = 0; i<args.length-1; i++ ){
+                numeros[i] = Integer.parseInt(args[i]);
+            }
+    fn(numeros, args[args.length-1]);
+                }else{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Cuantos números quiere introducir:");
+            int largo = Integer.parseInt(sc.nextLine());
+            int[] numeros = new int[largo];
+            for (int i = 0; i < numeros.length; i++) {
+                System.out.println("Introduzca el " + (i + 1) + "° número:");
+                numeros[i] = Integer.parseInt(sc.nextLine());
+            }
+            System.out.println("¿En que orden desea ordenar el intervalo?\na)creciente\nb)decreciente");
+            String respuesta = sc.nextLine();
+            fn(numeros, respuesta);
+
+            }
+        }
+
+
+    private static void fn(int[] num, String letra) {
+            switch (letra) {
+                case "a" -> System.out.println(Arrays.toString(ordenarCreciente(num)));
+                case "b" -> System.out.println(Arrays.toString(ordenarDecreciente(num)));
+                default -> System.out.println("Entrada Incorrecta, por favor responder con a o b");
+        }
+    }
+
+            private static int[] ordenarCreciente ( int[] numeros){
+                boolean completo = false;
+                while (!completo) {
+                    boolean[] ordenado = new boolean[numeros.length - 1];
+                    for (int e = 0; e < numeros.length - 1; e++) {
+                        for (int i = 0; i < numeros.length - 1; i++) {
+                            if (numeros[i] > numeros[i + 1]) {
+                                int tmp = numeros[i];
+                                numeros[i] = numeros[i + 1];
+                                numeros[i + 1] = tmp;
+                            }
+                        }
+                        ordenado[e] = true;
+                    }
+                    completo = estaOrdenado(ordenado);
+                }
+                return numeros;
+            }
+
+            private static int[] ordenarDecreciente ( int[] numeros){
+                boolean completo = false;
+                while (!completo) {
+                    boolean[] ordenado = new boolean[numeros.length - 1];
+                    for (int e = 0; e < numeros.length - 1; e++) {
+                        for (int i = 0; i < numeros.length - 1; i++) {
+                            if (numeros[i] < numeros[i + 1]) {
+                                int tmp = numeros[i];
+                                numeros[i] = numeros[i + 1];
+                                numeros[i + 1] = tmp;
+                            }
+                        }
+                        ordenado[e] = true;
+                    }
+                    completo = estaOrdenado(ordenado);
+                }
+                return numeros;
+            }
+
+
+            private static boolean estaOrdenado ( boolean[] ordenado) {
+                boolean completo = false;
+                for (int i = 0; i < ordenado.length - 1; i++) {
+                    if (i == 0) {
+                        completo = ordenado[i];
+                    } else {
+                        completo = completo && ordenado[i];
+                    }
+
+                }
+                return completo;
+            }
+}
