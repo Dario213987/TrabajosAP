951580bddd42ef0f7e56c73368b28e66c5f5c39d
Dario213987 <dariocrsng@gmail.com>
2023-03-13 07:54:14 -0300

    nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/uiDesigner.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_1_a/ejercicio_1_a.iml 	nuevos archivos: Trabajo 4/ejercicio_1_a/src/ejercicio_1_A.java 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_1_b/ejercicio_1_b.iml 	nuevos archivos: Trabajo 4/ejercicio_1_b/src/Main.java 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/.gitignore 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/misc.xml 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/modules.xml 	nuevos archivos: Trabajo 4/ejercicio_3/.idea/vcs.xml 	nuevos archivos: Trabajo 4/ejercicio_3/ejercicio_3.iml 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_2/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_3/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/out/production/ejercicio_3/out.txt 	nuevos archivos: Trabajo 4/ejercicio_3/src/Main.java 	nuevos archivos: Trabajo 4/ejercicio_3/src/in.txt 	nuevos archivos: Trabajo 4/ejercicio_3/src/out.txt

---
+++ Trabajo 4/src/ejercicio_1_A.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class ejercicio_1_A {
+
+    //args : num1 num2 num3 letra(a o b)
+    public static void main(String[] args) {
+        int[] numeros = new int[args.length-1];
+        String ascendiente = "A";
+        String descendiente = "D";
+        for(int i= 0; i < args.length-1; i++){
+            numeros[i] = Integer.parseInt(args[i]);
+        }
+        switch (args[3]) {
+            case "a" -> System.out.println(ascendiente+": "+ Arrays.toString(ordenarCreciente(numeros)));
+            case "b" -> System.out.println(descendiente+": "+ Arrays.toString(ordenarDecreciente(numeros)));
+        }
+
+
+    }
+
+    private static int[] ordenarCreciente(int[] numeros) {
+        boolean completo = false;
+        while (!completo) {
+            boolean[] ordenado = new boolean[numeros.length - 1];
+            for(int e = 0; e< numeros.length-1;e++) {
+                for (int i = 0; i  <numeros.length - 1; i++) {
+                    if (numeros[i] > numeros[i + 1]) {
+                        int tmp = numeros[i];
+                        numeros[i] = numeros[i + 1];
+                        numeros[i + 1] = tmp;
+                    }
+                }
+                ordenado[e] = true;
+            }
+            completo = estaOrdenado(ordenado);
+        }
+        return numeros;
+    }
+
+    private static int[] ordenarDecreciente(int[] numeros) {
+        boolean completo = false;
+        while (!completo) {
+            boolean[] ordenado = new boolean[numeros.length - 1];
+            for(int e = 0; e< numeros.length-1;e++) {
+                for (int i = 0; i < numeros.length - 1; i++) {
+                    if (numeros[i] < numeros[i + 1]) {
+                        int tmp = numeros[i];
+                        numeros[i] = numeros[i + 1];
+                        numeros[i + 1] = tmp;
+                    }
+                }
+                ordenado[e] = true;
+            }
+            completo = estaOrdenado(ordenado);
+        }
+        return numeros;
+    }
+
+
+    private static boolean estaOrdenado(boolean[] ordenado) {
+        boolean completo = false;
+        for (int i = 0; i < ordenado.length - 1; i++) {
+            if (i == 0) {
+                completo = ordenado[i];
+            } else {
+                completo = completo && ordenado[i];
+            }
+
+        }
+        return completo;
+    }
+}
